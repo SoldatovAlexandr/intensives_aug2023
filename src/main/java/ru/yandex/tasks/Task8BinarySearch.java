174d51e9b39ebd3ddca055a3ac6725a2f75bb3ab
Alexandr Soldatov <asoldatov@vp.ru>
2023-08-14 20:26:09 +0300

    Task8BinarySearch

--- src/main/java/ru/yandex/tasks/Task8BinarySearch.java
+++ src/main/java/ru/yandex/tasks/Task8BinarySearch.java
@@ -2,13 +2,17 @@ package ru.yandex.tasks;
 
 public class Task8BinarySearch {
     public static int find(int number, int[] sortedList) {
-        /*
-         * number: целое число, -10^5 <= number <= 10^5
-         * numbers: массив целых чисел, 0 <= numbers.length <= 10^5 (числа не повторяются)
-         * Выход: i, где numbers[i] = number, -1, если такого i нет
-         * Если таких i несколько, вывести наибольший
-         */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+        int left = 0, right = sortedList.length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (sortedList[middle] == number) {
+                return middle;
+            } else if (sortedList[middle] > number) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
         return -1;
     }
 
