0d64d33ccf325843eaa76dbd906a11b2a6211fff
Alexandr Soldatov <asoldatov@vp.ru>
2023-08-14 19:00:10 +0300

    Task3InsertionSort

--- src/main/java/ru/yandex/tasks/Task3InsertionSort.java
+++ src/main/java/ru/yandex/tasks/Task3InsertionSort.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 
 public class Task3InsertionSort {
     public static ArrayList<Integer> sort(ArrayList<Integer> numbers) {
-        /*
-         * numbers: массив целых чисел, -10^5 <= numbers[i] <= 10^5, длина массива до 10^5
-         * Выход: отсортированный (сортировкой вставками!) numbers
-         */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        for (int i = 1, j; i < numbers.size(); i++) {
+            int element = numbers.get(i);
+            for (j = i; j > 0 && element < numbers.get(j - 1); j--) {
+                numbers.set(j, numbers.get(j - 1));
+            }
+            numbers.set(j, element);
+        }
+        return numbers;
     }
 
     public static void selfCheck() {
